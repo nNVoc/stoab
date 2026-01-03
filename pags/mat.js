79a27de1698cf4850e9d8ddc261201250083201a
nNVoc <viniciuspolettis11062010@gmail.com>
2026-01-03 17:56:34 -0300

    melhorando

--- pags/mat.js
+++ pags/mat.js
@@ -1,4 +1,5 @@
 var itens = document.querySelectorAll("ul>li");
+var li2nds = document.querySelectorAll(".li-2nd");
 
 itens.forEach((item, i) => {
     var checkbox = document.createElement("input");
@@ -10,11 +11,50 @@ itens.forEach((item, i) => {
     item.innerText = '';
     item.setAttribute("class", "item")
     item.setAttribute("id", "itemi-" + i)
-    item.appendChild(label);
     label.appendChild(checkbox);
+    item.appendChild(label);
 
     checkbox.addEventListener("change", () => {
         item.classList.toggle("checked", checkbox.checked)
     })
 })
+
+li2nds.forEach((li2nd, j) => {
+    var article = document.createElement("article");
+    article.id = "article-" + j;
+    article.setAttribute("class", "article-2nd");
+    li2nd.appendChild(article);
+})
+
+/* Lê aí o gpt 
+const counter = document.getElementById("counter");
+const items = document.querySelectorAll(".item");
+
+function atualizar() {
+    const mudados = document.querySelectorAll(".item.changed").length;
+    counter.textContent = `${mudados}/${items.length}`;
+}
+
+// observa mudanças de classe
+const observer = new MutationObserver(mutations => {
+    for (const m of mutations) {
+        if (m.type === "attributes" && m.attributeName === "class") {
+            atualizar();
+            break;
+        }
+    }
+});
+
+// começa a observar
+items.forEach(item => {
+    observer.observe(item, {
+        attributes: true,
+        attributeFilter: ["class"]
+    });
+});
+
+// inicial
+atualizar();
+ */
+
 
