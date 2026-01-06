3f16b3b6bdd373e4ce025fd06fd8790ee72202bd
nNVoc <viniciuspolettis11062010@gmail.com>
2026-01-06 12:50:24 -0300

    química

--- pags/mat.js
+++ pags/mat.js
@@ -8,7 +8,10 @@ function setchecked(key) {
 
 const maior = [...itens].reduce((a,b) => b.innerText.length > a.innerText.length ? b : a);
 document.querySelectorAll("ul").forEach(ul => {
-    ul.style.gridAutoRows = `${maior.innerText.length * 1.2}px`;
+    ul.style.gridAutoRows = `${maior.innerText.length * 1.3}px`;
+    if (window.innerWidth >= 1600) {
+        ul.style.gridAutoRows = `80px`
+    }
 })
 
 itens.forEach((item, i) => {
